7e4b7e6198741a04ac1b4e5f3bee7bab0d3d3a50
l bates childs <labchilds@gmail.com>
2022-02-04 14:50:11 -0500

    add role and employee tables, add seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,26 @@
-DROP TABLE IF EXISTS departments; 
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department; 
 
-CREATE TABLE departments (
+CREATE TABLE department (
     id INTEGER PRIMARY KEY AUTO_INCREMENT,
     _name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id  INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    fist_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,31 @@
-INSERT INTO departments (_name)
+INSERT INTO department (_name)
 VALUES 
 ('Engineering'), 
 ('Design'), 
 ('Sales'), 
 ('Accounting');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Manager', 100000, NULL),
+('Project Manager', 90000, 1),
+('Engineer', 80000, 1),
+('Art Director', 80000, 2),
+('Designer', 60000, 2),
+('Account Manager', 60000, 3),
+('Sales Person', 45000, 3),
+('Accountant', 60000, 4);
+
+INSERT INTO employee (fist_name, last_name, role_id, manager_id)
+VALUES
+('Natalie', 'Goodman', 1, NULL),
+('Linzi', 'Oakley', 2, 1),
+('Tasnia', 'Chadwick', 3, 2),
+('Bianca', 'Goddard', 3, 2),
+('Elias', 'Griffin', 4, 1),
+('Cian', 'Bullock', 5, 5),
+('Louisa', 'Kaur', 1, NULL),
+('Yazmin', 'Cassidy', 8, 7),
+('Anna-Marie', 'Andersen', 6, 7),
+('Ezekial', 'Sampson', 7, 9),
+('Tanvir', 'Churchill', 7, 9)
