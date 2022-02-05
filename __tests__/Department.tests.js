815b5b7dd3976da9463157f57d9eed0072fd07f1
l bates childs <labchilds@gmail.com>
2022-02-05 15:41:36 -0500

    renamed file

--- __tests__/Department.tests.js
+++
@@ -1,8 +0,0 @@
-const Department = require('../lib/Department');
-
-// check that Dept class creates a dept obj
-test('check Department class create obj with properties', () => {
-    const dept = new Department('Finance');
-
-    expect(dept.name).toBe('Finance');
-});
