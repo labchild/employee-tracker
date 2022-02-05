1b7408c8935a57b26a3b5c1c9f3b6db7f2d8b923
l bates childs <labchilds@gmail.com>
2022-02-04 19:19:14 -0500

    ad employee routes (get all, post, put/update role)

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const deptRoutes = require('./dept-routes');
 const roleRoutes = require('./role-routes');
+const employeeRoutes = require('./employee-routes');
 
 router.use('/depts', deptRoutes);
 router.use('/roles', roleRoutes);
+router.use('/employees', employeeRoutes);
 
 module.exports = router;
